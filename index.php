9778226f7beef3659a6a171bab7b95bd6945d2d7
yann-app <yannochh@gmail.com>
2020-12-03 18:05:01 +0100

    premier site

---
+++ index.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <title>Wineadventure</title>
+</head>
+<body>
+
+            <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase pr-5" href="#">Wine adventure</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link text-uppercase px-2" href="#">Accueil <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase px-2" href="#">Presentation</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase px-2" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase px-2" href="#">Tarif</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-uppercase px-2" href="#">Contact</a>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+            <div class="container-full">
+                <div class="text">
+                    <h1>Voyages en France</h1>
+                        <button type="button" class="btn btn-secondary">Cliquez ici</button>
+                </div>
+            </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+            <h2 class="text-center">Lorem Ipsum</h2>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-4 d-flex justify-content-center">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+            <div class="col-md-4 d-flex justify-content-center">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-center">
+        <div class="col-md-4">
+            <img class="blois" src="images/blois_1.jpg">
+        </div>
+            <div class="col-md-4">
+                <img class="chen" src="images/chenonceaux_1.jpg">
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-4">
+                <img class="blois" src="images/blois_1.jpg">
+            </div>
+            <div class="col-md-4">
+                <img class="chen" src="images/chenonceaux_1.jpg">
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <footer>
+
+        </footer>
+    </div>
+
+
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+<!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+-->
+</body>
+</html>
